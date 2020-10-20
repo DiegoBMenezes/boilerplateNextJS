ec54ed622e9f6fde1afdade91f18de784ff813ee
Diego Menezes <barreto.di@gmail.com>
2020-10-20 14:18:19 -0300

    melhorias snapshot jest e style components

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
